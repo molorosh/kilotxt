f57da4910137c18de094eb065544308c2bb1af14
Howard Ricketts <molorosh@gmail.com>
2022-01-26 18:20:13 +0000

    Step 007 completed

--- kilo.c
+++ kilo.c
@@ -1,11 +1,26 @@
+
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(){
+    // re-applies the user's previous terminal settings
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode(){
+    // saves a copy of the user's current terminal settings
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    // create a new copy of the current settings
+    struct termios raw = orig_termios;
     // turns off automatic echoing of keystrokes to STDIN
-    struct termios raw;
     tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
+    // disable ECHO : stops typing appearing in terminal
+    // disable ICANON : change from by-line to by-key
+    raw.c_lflag &= ~(ECHO|ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
